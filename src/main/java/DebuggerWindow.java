d121cafc67fc36aea863c87c582543c8dd5f5129
Richard Lesh <90526226+richlesh@users.noreply.github.com>
2026-06-29 22:09:16 -0500

    Use cell padding instead of intercell spacing in register table

--- src/main/java/DebuggerWindow.java
+++ src/main/java/DebuggerWindow.java
@@ -211,8 +211,16 @@ public class DebuggerWindow extends JFrame {
         };
         regTable.setFont(monoFont);
         regTable.getTableHeader().setFont(monoFont);
-        regTable.setIntercellSpacing(new Dimension(4, 2));
         regTable.setRowHeight(regTable.getRowHeight() + 4);
+        javax.swing.table.DefaultTableCellRenderer padRenderer = new javax.swing.table.DefaultTableCellRenderer() {
+            @Override public Component getTableCellRendererComponent(JTable t, Object v, boolean s, boolean f, int r, int c) {
+                Component comp = super.getTableCellRendererComponent(t, v, s, f, r, c);
+                setBorder(BorderFactory.createEmptyBorder(2, 4, 2, 4));
+                return comp;
+            }
+        };
+        padRenderer.setFont(monoFont);
+        for (int i = 0; i < regTable.getColumnCount(); i++) regTable.getColumnModel().getColumn(i).setCellRenderer(padRenderer);
         regTable.getColumnModel().getColumn(0).setPreferredWidth(40);
         regTable.getColumnModel().getColumn(1).setPreferredWidth(140);
         regTable.getColumnModel().getColumn(2).setPreferredWidth(40);
